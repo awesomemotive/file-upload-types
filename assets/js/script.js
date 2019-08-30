16c00731011cac0528c8164c5cd1f281d9699f84
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-08-30 21:10:31 +0545

    Clone and remove functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 
 jQuery( document ).ready( function( $ ) {
 	$( document ).on( 'click', '#file-upload-types .table-container .dashicons-plus', function( e ) {
+		e.preventDefault();
 
+		var closest = $( this ).closest( 'tr' );
+		var clone 	= closest.clone();
+		clone.find( 'input' ).attr( 'value', '' );
+
+		closest.after( clone );
+	});
+
+	$( document ).on( 'click', '#file-upload-types .table-container .dashicons-minus', function( e ) {
+		e.preventDefault();
+
+		$( this ).closest( 'tr' ).remove();
 	});
 });

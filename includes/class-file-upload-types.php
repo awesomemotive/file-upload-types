3925b1f521d8e4b56b67c8ab52cd31826479a4c2
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-09-16 11:55:41 +0545

    Core functionality of uploading extra types

--- includes/class-file-upload-types.php
+++ includes/class-file-upload-types.php
@@ -47,6 +47,8 @@ final class File_Upload_Types {
 		// Load plugin text domain.
 		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
 		add_filter( 'plugin_action_links_'. plugin_basename( FILE_UPLOAD_TYPES_PLUGIN_FILE ), array( $this, 'plugin_action_links' ) );
+		add_filter( 'upload_mimes', array( $this, 'allowed_types' ) );
+		add_filter( 'wp_check_filetype_and_ext', array( $this, 'real_file_types' ), 10, 5 );
 
 		$this->define_constants();
 		$this->includes();
@@ -102,5 +104,49 @@ final class File_Upload_Types {
 			include_once dirname( __FILE__ ) . '/class-file-upload-types-settings.php';
 			include_once dirname( __FILE__ ) . '/file-upload-types-functions.php';
 		}
+	}
+
+	/**
+	 * File types allowed to upload.
+	 *
+	 * @param  $mime_types array  List of all mime allowed mime types.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/wp_get_mime_types/
+	 *
+	 * @return array
+	 *
+	 * @since  1.0.0
+	 */
+	public function allowed_types( $mime_types ) {
+		$enabled_types 		= get_option( 'file_upload_types', array() );
+		$add_mime_types	   	= array();
+
+		foreach( $enabled_types as $type ) {
+
+			if ( empty( $type['ext'] ) || empty( $type['mime'] ) ) {
+				continue;
+			}
+
+			$add_mime_types[ $type['ext'] ] = $type['mime'];
+		}
+
+		return array_merge( $mime_types, $add_mime_types );
+	}
+
+	/**
+	 * Filters the “real” file type of the given file.
+	 *
+	 * @param array 	$wp_check_filetype_and_ext 	File data array containing 'ext', 'type', and 'proper_filename' keys.
+	 * @param string 	$file 						Full path to the file.
+	 * @param string 	$filename 					The name of the file (may differ from $file due to $file being in a tmp directory).
+	 * @param array 	$mimes 						Key is the file extension with value as the mime type.
+	 * @param string|bool $real_mime 				The actual mime type or false if the type cannot be determined.
+	 *
+	 * @return array.
+	 *
+	 * @since  1.0.0
+	 */
+	public function real_file_types ( $wp_check_filetype_and_ext, $file, $filename, $mimes, $real_mime ) {
+
 	}
 }

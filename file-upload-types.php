9905ea664f3683a217db4dd8788c37fd9f8c3d00
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2019-12-17 10:49:03 +0545

    Requires PHP 5.6 or greater.

--- file-upload-types.php
+++ file-upload-types.php
@@ -12,6 +12,41 @@
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
 /**
+ * Requires PHP 5.6.0+.
+ */
+if ( version_compare( PHP_VERSION, '5.6.0', '<' ) ) {
+
+	/**
+	 * Deactivate plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	function file_upload_types_deactivate() {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	}
+	add_action( 'admin_init', 'file_upload_types_deactivate' );
+
+	/**
+	 * Display notice after deactivation.
+	 *
+	 * @since 1.0.0
+	 */
+	function file_upload_types_deactivate_msg() {
+
+		echo '<div class="notice notice-error"><p>';
+		echo esc_html__( 'The File Upload Types plugin has been deactivated. Your site is running an outdated version of PHP that is no longer supported and is not compatible with the File Upload Types plugin.', 'file-upload-types' );
+		echo '</p></div>';
+
+		if ( isset( $_GET['activate'] ) ) { // WPCS: CSRF ok.
+			unset( $_GET['activate'] ); // WPCS: CSRF ok.
+		}
+	}
+	add_action( 'admin_notices', 'file_upload_types_deactivate_msg' );
+
+	return;
+}
+
+/**
  * Plugin constants.
  */
 define( 'FILE_UPLOAD_TYPES_PLUGIN_FILE', __FILE__ );

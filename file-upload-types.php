7d02fe106a6d9fc48c53a8fc8011d171cb35ec26
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2020-01-06 22:00:09 +0545

    Requires PHP 5.6 or greater, fix #2.
    * Requires PHP 5.6 or greater.
    
    * minor adjustments, fix #2.
    
    Co-authored-by: Slava Abakumov <slaFFik@users.noreply.github.com>

--- file-upload-types.php
+++ file-upload-types.php
@@ -12,6 +12,48 @@
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
 /**
+ * The plugin requires PHP 5.6.0+.
+ * It will self-deactivate on older PHP versions ad will notify an admin.
+ */
+if ( version_compare( PHP_VERSION, '5.6.0', '<' ) ) {
+
+	/**
+	 * Deactivate the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	function file_upload_types_deactivate() {
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	}
+	add_action( 'admin_init', 'file_upload_types_deactivate' );
+
+	/**
+	 * Display a notice after deactivation.
+	 *
+	 * @since 1.0.0
+	 */
+	function file_upload_types_deactivate_msg() {
+
+		// Display the message to admin only.
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		echo '<div class="notice notice-error"><p>';
+		echo esc_html__( 'The File Upload Types plugin has been deactivated. Your site is running an outdated version of PHP that is no longer supported and is not compatible with the File Upload Types plugin.', 'file-upload-types' );
+		echo '</p></div>';
+
+		if ( isset( $_GET['activate'] ) ) { // WPCS: CSRF ok.
+			unset( $_GET['activate'] ); // WPCS: CSRF ok.
+		}
+	}
+	add_action( 'admin_notices', 'file_upload_types_deactivate_msg' );
+
+	return;
+}
+
+/**
  * Plugin constants.
  */
 define( 'FILE_UPLOAD_TYPES_PLUGIN_FILE', __FILE__ );
